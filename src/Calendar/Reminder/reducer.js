d2771628a08ff74ee51020df6e500086302712d9
Guillermo Garcia <bitcoder@gmail.com>
2020-10-28 19:12:12 -0500

    Adding generic reducer for Reminder

---
+++ src/Calendar/Reminder/reducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+  reminder: {},
+  actionFailure: false,
+};
+
+const reducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+
+    default:
+      return state;
+
+  }
+};
+
+export default reducer;
