28ccf7f4264bd4cd7ddc45444a1ebf4a35c90f27
Guillermo Garcia <bitcoder@gmail.com>
2020-10-30 01:05:33 -0500

    Styling Calendar Header component

--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import CalendarHeader from "./CalendarHeader"
 import CalendarMonth from "./CalendarMonth";
+import moment from "moment";
 
 class Calendar extends React.Component {
 
     render() {
       return(    
         <>
-            <CalendarHeader currentMonth="October" />
+            <CalendarHeader currentMonth={ moment().format("MMMM") } />
             <CalendarMonth />
         </>
       );
--- src/Calendar/CalendarHeader.jsx
+++ src/Calendar/CalendarHeader.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import "./CalendarHeader.css"
+
 const CalendarHeader = ({ currentMonth }) => (
         <div className="calendar-header">
             <span><button> &lt;&lt; </button></span>
-            <span>{ currentMonth }</span>
+            <span><strong>{ currentMonth }</strong></span>
             <span><button> &gt;&gt; </button></span>
         </div>
 );
