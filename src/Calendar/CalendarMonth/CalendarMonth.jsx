c1663f3ace4e2a7652ef50a580546dd976ac3b15
Guillermo Garcia <bitcoder@gmail.com>
2020-10-30 02:56:58 -0500

    Logic and Styles for CalendarMonth component. Mocked Reminders

--- src/Calendar/CalendarMonth/CalendarMonth.jsx
+++ src/Calendar/CalendarMonth/CalendarMonth.jsx
@@ -13,50 +13,124 @@ const CalendarMonth = () => {
       </th>
     );
   });
+  
+  // Getting days before the current month
+  function getDaysBefore() {
+    const firstDayOfMonth = () => {
+      let firstDay = moment()
+                   .startOf("month")
+                   .format("d"); 
+     return firstDay;
+    };
+  
+    let emptyDaysBefore = [];
+    let i = firstDayOfMonth();
+    
+    while(i > 0) {
+      let dayBeforeObject = moment()
+                      .startOf('month')
+                      .subtract(i, 'days');
 
-  const firstDayOfMonth = () => {
-    let dateObject = moment();
-    let firstDay = moment(dateObject)
-                 .startOf("month")
-                 .format("d"); 
-   return firstDay;
-  };
+      let dayBefore = dayBeforeObject.format('D');
+      let dayNumberInWeek = moment(dayBeforeObject,'D').format("d");
+      let isWeekend = (dayNumberInWeek == 0 || dayNumberInWeek == 6) ? 'weekend-day' : '';
 
-  let blanks = [];
-  for (let i = 0; i < firstDayOfMonth(); i++) {
-    blanks.push(
-      <td className="calendar-day empty">{""}</td>
-    );
+      emptyDaysBefore.push(
+        <td key={"empty-" + i}  className={"calendar-day empty" + " " + isWeekend}>
+          <div className="calendar-day-container">
+            <div className="calendar-day-number">{ dayBefore }</div>
+          </div>
+        </td>
+      );
+
+      i--;
+    }
+
+    return emptyDaysBefore;
   }
 
-  let daysInMonth = [];
-  for (let d = 1; d <= moment().daysInMonth(); d++) {
-    daysInMonth.push(
-      <td key={d} className="calendar-day">
-        {d}
-      </td>
-    );
+  // Getting days after the current month
+  function getDaysAfter() {
+    const lastDayOfMonth = () => {
+      let lastDay = moment()
+                   .endOf("month")
+                   .format("d"); 
+     return lastDay;
+    };
+  
+    let emptyDaysAfter = [];
+    let i = lastDayOfMonth();
+
+    while(i < 6) {
+      let dayAfterObject = moment()
+                        .endOf('month')
+                        .add(i, 'days');
+
+      let dayAfter = dayAfterObject.format('D');
+      let dayNumberInWeek = moment(dayAfterObject,'D').format("d");
+      let isWeekend = (dayNumberInWeek == 0 || dayNumberInWeek == 6) ? 'weekend-day' : '';
+  
+      emptyDaysAfter.push(
+        <td key={"empty-" + i}  className={"calendar-day empty" + " " + isWeekend}>
+          <div className="calendar-day-container">
+            <div className="calendar-day-number">{ dayAfter }</div>
+          </div>
+        </td>
+      );
+
+      i++
+    }
+
+    return emptyDaysAfter;
   }
 
-  var totalSlots = [...blanks, ...daysInMonth];
+  // Getting days of the current month
+  function getDaysinMonth() {
+    let daysCurrentMonth = [];
+    for (let d = 1; d <= moment().daysInMonth(); d++) {
+
+      let dayNumberInWeek = moment(d,'D').format("d");
+      let isWeekend = (dayNumberInWeek == 0 || dayNumberInWeek == 6) ? 'weekend-day' : '';
+
+      daysCurrentMonth.push(
+        <td key={"day-" + d} className={"calendar-day-" + d + " " + isWeekend}>
+          <div className="calendar-day-container">
+            <div className="calendar-day-number">{d}</div>
+            <div className="calendar-reminders-container">
+              <div style={{ background: "#2fa" }}>13:50 <span>Notif... 1</span></div>
+              <div style={{ background: "#a90" }}>16:30 <span>Notif... 2</span></div>
+            </div>
+          </div>
+        </td>
+      );
+    }
+
+    return daysCurrentMonth;
+  }
+
+  let daysBefore = getDaysBefore();
+  let daysInMonth = getDaysinMonth();
+  let daysAfter = getDaysAfter();
+  var totalSlots = [...daysBefore, ...daysInMonth, ...daysAfter];
   let rows = [];
   let cells = [];
 
+  // Filling out rows and cells into the slot
   totalSlots.forEach((row, i) => {
     if (i % 7 !== 0) {
-      cells.push(row); // if index not equal 7 that means not go to next week
+      cells.push(row);
     } else {
-      rows.push(cells); // when reach next week we contain all td in last week to rows 
-      cells = []; // empty container 
-      cells.push(row); // in current loop we still push current row to new container
+      rows.push(cells);
+      cells = [];
+      cells.push(row);
     }
-    if (i === totalSlots.length - 1) { // when end loop we add remain date
+    if (i === totalSlots.length - 1) { 
       rows.push(cells);
     }
   });
 
-  let daysinmonth = rows.map((d, i) => {
-    return <tr>{d}</tr>;
+  let calendarDates = rows.map((d, i) => {
+    return <tr key={"column-day-" + i} className={i}>{d}</tr>;
   });
 
   const wrapped = (dataGrid) => (
@@ -68,19 +142,15 @@ const CalendarMonth = () => {
   return wrapped(
     <table className="calendar-table">
       <thead>
-        <tr>
-          {weekDays}
-        </tr>
+        <tr>{weekDays}</tr>
       </thead>
 
       <tbody>
-      {daysinmonth}
+        {calendarDates}
       </tbody>
     </table>,
   );
 };
-
-
 
 const mapReduxStateToProps = state => ({
   data: state,
